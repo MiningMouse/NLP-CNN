9f181e2a2adaad38183cc5653b26331b1088741d
Austin Dress <austin.dress@jhuapl.edu>
2016-08-15 08:38:30 -0400

    More documentation.

--- python/twitter_sent/bow_train_test.py
+++ python/twitter_sent/bow_train_test.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-#  Author: Austin Dress
-#  Date: 6/26/16
+#  Author: Angela Chapman
+#  Date: 8/6/2014
 #
-#  This file is for training and testing on the twitter
-#  sentiment140 dataset using bow of words(BOW)
+#  This file contains code to accompany the Kaggle tutorial
+#  "Deep learning goes to the movies".  The code in this file
+#  is for Part 1 of the tutorial on Natural Language Processing.
 #
 # *************************************** #
 
@@ -15,95 +16,136 @@ from KaggleWord2VecUtility import KaggleWord2VecUtility
 import pandas as pd
 import numpy as np
 import nltk
-import pickle
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+from sklearn import metrics
 
-import re
+classes = ['0','1']
 
-# compile regular expressions that match repeated characters and emoji unicode
-emoji = re.compile(u'[^\x00-\x7F\x80-\xFF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF]',re.UNICODE)
-multiple = re.compile(r"(.)\1{1,}", re.DOTALL)
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, weight='bold')
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    #plt.xticks(tick_marks, self.classes, rotation=45)
+    plt.xticks(tick_marks, classes, rotation='vertical')
+    plt.yticks(tick_marks, classes)
+    plt.tight_layout()
+    plt.ylabel('True label', weight='bold')
+    plt.xlabel('Predicted label', weight='bold')
 
-def format(tweet):
 
-    #strip emoji
-    stripped = emoji.sub('',tweet)
+def show_confusion_matrix(l_test, predicted):
+    # Compute confusion matrix
+    cm = metrics.confusion_matrix(l_test, predicted)
+    np.set_printoptions(precision=2)
+    print('Confusion matrix, without normalization')
+    print(cm)
+    plot_confusion_matrix(cm)
 
-    #strip URLs
-    stripped = re.sub(r'http[s]?[^\s]+','', stripped)
-
-    #strip "@name" components
-    stripped = re.sub(r'(@[A-Za-z0-9\_]+)' , "" ,stripped)
-
-    #strip html '&amp;', '&lt;', etc.
-    stripped = re.sub(r'[\&].*;','',stripped)
-
-    #strip punctuation
-    stripped = re.sub(r'[#|\!|\-|\+|:|//]', " ", stripped)
-
-    #strip the common "RT"
-    stripped = re.sub( 'RT.','', stripped)
-
-    #strip whitespace down to one.
-    stripped = re.sub('[\s]+' ,' ', stripped).strip()
-
-    #strip multiple occurrences of letters
-    stripped = multiple.sub(r"\1\1", stripped)
-
-    #strip all non-latin characters
-    #if we wish to deal with foreign language tweets, we would need to first
-    #translate them before taking this step.
-
-    stripped = re.sub('[^a-zA-Z0-9|\']', " ", stripped).strip()
-
-    return stripped
+    # Normalize the confusion matrix by row (i.e by the number of samples
+    # in each class)
+    cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    # print('Safety Success - Normalized confusion matrix')
+    print(cm_normalized)
+    plot_confusion_matrix(cm_normalized)
+    plt.show()
 
 data_path = '/home/dressag1/Documents/Datasets/imdb_movie/'
 if __name__ == '__main__':
 
-    print "Loading the data"
-    # Getting back the objects:
-    with open('sent_labels_cleaned2.pickle', 'rb') as f:  # Python 3: open(..., 'rb')
-        d_train, l_train, d_test, l_test = pickle.load(f)
-
-    # print "Combining words into sentences for BOW"
-    # d_train_sen = []
-    # for d in d_train:
-    #     tweet = format(' '.join(d))
-    #     d_train_sen.append(tweet)
-    #
-    # d_test_sen = []
-    # for d in d_test:
-    #     tweet = format(' '.join(d))
-    #     d_test_sen.append(tweet)
+    print ('Loading the data')
+    print("Loading data...")
+    neg_train_path = '../data/twitter_train_100000_and_test.neg'
+    pos_train_path = '../data/twitter_train_100000_and_test.pos'
 
 
-    # train = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', data_path +'labeledTrainData.tsv'), header=0, \
-    #                 delimiter="\t", quoting=3)
-    # test = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', data_path + 'testData.tsv'), header=0, delimiter="\t", \
-    #                quoting=3 )
+    f_pos = open(pos_train_path, 'r')
+    f_neg = open(neg_train_path,'r')
+
+    # Initialize an empty list to hold the clean reviews
+    clean_train_reviews = []
+    labels = []
+    # Loop over each review; create an index i that goes from 0 to the length
+    # of the movie review list
+    print "Cleaning and parsing the training set movie reviews...\n"
+    # first do the positives
+    reviews = f_pos.readlines()
+    for review in reviews:
+        # pass in the review, clean, and get rid of stop words
+        clean_train_reviews.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(review, True)))
+        labels.append(1)
+
+
+    # clean the negative reviews
+    reviews = f_neg.readlines()
+    for review in reviews:
+        # pass in the review, clean, and get rid of stop words
+        clean_train_reviews.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(review, True)))
+        labels.append(0)
+
+    # generate shuffle indices
+    shuffle_indices = np.array(np.random.permutation(np.arange(len(clean_train_reviews))))
+
+    # convert to numpy array so we can shuffle
+    clean_train_reviews = np.array(clean_train_reviews)
+    labels = np.array(labels)
+
+    # shuffle the data
+    clean_train_reviews = clean_train_reviews[shuffle_indices]
+    labels = labels[shuffle_indices]
+
+    # define train, test
+    train_n = 100000
+
+    # test data
+    # d_test = clean_train_reviews[train_n:]
+    # l_test = labels[train_n:]
     #
-    # print 'The first review is:'
-    # print train["review"][0]
+    # # train data
+    # d_train = clean_train_reviews[0:train_n]
+    # l_train = labels[0:train_n]
+
+
+    d_test = []
+    l_test = []
+    d_train =[]
+    l_train = []
+
+    # extract training and test data
+    d_test.extend(clean_train_reviews[train_n:train_n + 182])
+    d_test.extend(clean_train_reviews[train_n + 182 + 100000:])
+
+    l_test.extend(labels[train_n:train_n + 182])
+    l_test.extend(labels[train_n + 182 + 100000:])
+
+    d_train.extend(clean_train_reviews[0:train_n])
+    d_train.extend(clean_train_reviews[train_n + 182:train_n + 182 + 100000])
+
+    l_train.extend(labels[0:train_n])
+    l_train.extend(labels[train_n + 182:train_n + 182 + 100000])
+
+
+    # convert back to numpy array
+    d_test = np.array(d_test)
+    l_test = np.array(l_test)
+
+    d_train = np.array(d_train)
+    l_train = np.array(l_train)
+
+    # d_test = np.vstack((clean_train_reviews[train_n:train_n + 182], clean_train_reviews[train_n + 182 + 100000:]))
+    # l_test = np.vstack((labels[train_n:train_n + 182], labels[train_n + 182 + 100000:]))
     #
-    # raw_input("Press Enter to continue...")
-    #
-    #
-    # print 'Download text data sets. If you already have NLTK datasets downloaded, just close the Python download window...'
-    # #nltk.download()  # Download text data sets, including stop words
-    #
-    # # Initialize an empty list to hold the clean reviews
-    # clean_train_reviews = []
-    #
-    # # Loop over each review; create an index i that goes from 0 to the length
-    # # of the movie review list
-    #
-    # print "Cleaning and parsing the training set movie reviews...\n"
-    # for i in xrange( 0, len(train["review"])):
-    #     clean_train_reviews.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(train["review"][i], True)))
-    #
-    #
+    # d_train = np.vstack((clean_train_reviews[0:train_n], clean_train_reviews[train_n + 182:train_n + 182 + 100000]))
+    # l_train = np.vstack((labels[0:train_n], labels[train_n + 182:train_n + 182 + 100000]))
+
+
+
+
+
+
     # ****** Create a bag of words from the training set
-
+    #
     print "Creating the bag of words...\n"
 
 
@@ -123,7 +165,7 @@ if __name__ == '__main__':
 
     # Numpy arrays are easy to work with, so convert the result to an
     # array
-    train_data_features = (train_data_features).toarray # changed from to_array() Memory Error
+    train_data_features = train_data_features.toarray()
 
     # ******* Train a random forest using the bag of words
     #
@@ -135,33 +177,28 @@ if __name__ == '__main__':
 
     # Fit the forest to the training set, using the bag of words as
     # features and the sentiment labels as the response variable
-    #
+
     # This may take a few minutes to run
     forest = forest.fit( train_data_features, l_train )
-    #
-    #
-    #
-    # # Create an empty list and append the clean reviews one by one
-    # clean_test_reviews = []
-    #
-    # print "Cleaning and parsing the test set movie reviews...\n"
-    # for i in xrange(0,len(test["review"])):
-    #     clean_test_reviews.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(test["review"][i], True)))
-    #
-    # # Get a bag of words for the test set, and convert to a numpy array
-    # test_data_features = vectorizer.transform(clean_test_reviews)
-    # test_data_features = test_data_features.toarray()
-    #
-    # # Use the random forest to make sentiment label predictions
-    # print "Predicting test labels...\n"
-    # result = forest.predict(test_data_features)
-    #
-    # # Copy the results to a pandas dataframe with an "id" column and
-    # # a "sentiment" column
-    # output = pd.DataFrame( data={"id":test["id"], "sentiment":result} )
-    #
-    # # Use pandas to write the comma-separated output file
-    # output.to_csv(os.path.join(os.path.dirname(__file__), 'data', data_path + 'Bag_of_Words_model.csv'), index=False, quoting=3)
-    # print "Wrote results to Bag_of_Words_model.csv"
-    #
-    #
+
+
+    print "Evaluating model on test data..."
+
+    # Get a bag of words for the test set, and convert to a numpy array
+    test_data_features = vectorizer.transform(d_test)
+    test_data_features = test_data_features.toarray()
+
+    # Use the random forest to make sentiment label predictions
+    print "Predicting test labels...\n"
+    pred = forest.predict(test_data_features)
+
+    print "Confusion Matrix"
+    cm = confusion_matrix(l_test, pred)
+
+    print cm
+    print "Confusion Matrix"
+    cm = confusion_matrix(l_test, pred)
+
+    print "Normalized Confusion Matrix"
+    show_confusion_matrix(l_test, pred)
+
